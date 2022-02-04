994366a089fd4f39fbda2c523564d0ccb51f9401
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-02-04 16:20:47 +0000

    add name searching

--- src/main/java/net/htmlcsjs/coffeeFloppa/commands/QuestbookCommand.java
+++ src/main/java/net/htmlcsjs/coffeeFloppa/commands/QuestbookCommand.java
@@ -1,6 +1,7 @@
 package net.htmlcsjs.coffeeFloppa.commands;
 
 import discord4j.core.object.entity.Message;
+import net.htmlcsjs.coffeeFloppa.FloppaLogger;
 import net.htmlcsjs.coffeeFloppa.helpers.QuestDefinition;
 import net.htmlcsjs.coffeeFloppa.helpers.Questbook;
 
@@ -25,6 +26,7 @@ public class QuestbookCommand implements ICommand {
         String verb = message.getContent().split(" ")[1];
         String[] splitArg = message.getContent().split(" ");
         String arg = String.join(" ", Arrays.copyOfRange(splitArg, 2, splitArg.length));
+        int questCount = 0;
         if (verb.equalsIgnoreCase("id")) {
             QuestDefinition quest = qb.questMap.get(Long.parseLong(arg));
             if (quest != null) {
@@ -32,6 +34,29 @@ public class QuestbookCommand implements ICommand {
             } else {
                 return "Sorry, that quest could not be found";
             }
+        } else if (verb.equalsIgnoreCase("search")) {
+            StringBuilder msgBuilder = new StringBuilder();
+            for (String questname: qb.nameMap.keySet()) {
+                for (String str: arg.split(" ")) {
+                    if (questname.toLowerCase().contains(str.toLowerCase())) {
+                        msgBuilder.append(qb.nameMap.get(questname))
+                                .append(": ")
+                                .append(questname)
+                                .append("\n");
+                        questCount++;
+                        break;
+                    }
+                }
+            }
+            if (msgBuilder.isEmpty()) {
+                msgBuilder.append("Sorry, we couldnt find any quests matching ")
+                        .append(arg)
+                        .append(" in the database for ")
+                        .append(qbName);
+            }
+            FloppaLogger.logger.info(msgBuilder.toString());
+            msgBuilder.insert(0, "Found " + questCount + " quests matching `" + arg + "`:```\n");
+            return msgBuilder.append("```").toString();
         }
         return "Sorry, that was an invalid input";
     }
