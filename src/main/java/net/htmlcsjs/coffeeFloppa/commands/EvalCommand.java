174b3650669d3d11f7086513154ce204cea9708d
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-02-12 09:30:45 +0000

    add some qol to eval and attachment useage

--- src/main/java/net/htmlcsjs/coffeeFloppa/commands/EvalCommand.java
+++ src/main/java/net/htmlcsjs/coffeeFloppa/commands/EvalCommand.java
@@ -2,9 +2,12 @@ package net.htmlcsjs.coffeeFloppa.commands;
 
 import discord4j.core.object.entity.Message;
 
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 
 public class EvalCommand implements ICommand{
@@ -22,10 +25,28 @@ public class EvalCommand implements ICommand{
             try {
                 FileWriter pyWriter = new FileWriter("tmp.py");
                 StringBuilder codeBuilder = new StringBuilder();
-                codeBuilder.append("import math\nimport numpy as np\nimport pandas as pd\n");
-                for (String str: arg.split("\\n")) {
-                    if (!str.toLowerCase().contains("import")) {
-                        codeBuilder.append(str);
+                codeBuilder.append("import math\nimport numpy as np\nimport pandas as pd\nimport matplotlib.pyplot as plt\nimport copy\n");
+                if (message.getAttachments().size() > 0) {
+                    URL url = new URL(message.getAttachments().get(0).getUrl());
+                    ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
+                    FileOutputStream fileOutputStream = new FileOutputStream("dirty.py");
+                    fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+                    fileOutputStream.close();
+
+                    String data = Files.readString(Path.of("dirty.py"));
+
+                    for (String strRaw : data.split("\n")) {
+                        String str = strRaw.replaceAll("`", "");
+                        if (!str.toLowerCase().contains("import")) {
+                            codeBuilder.append(str).append("\n");
+                        }
+                    }
+                } else {
+                    for (String strRaw : arg.split("\n")) {
+                        String str = strRaw.replaceAll("`", "");
+                        if (!str.toLowerCase().contains("import")) {
+                            codeBuilder.append(str).append("\n");
+                        }
                     }
                 }
                 pyWriter.write(codeBuilder.toString());
@@ -35,7 +56,9 @@ public class EvalCommand implements ICommand{
                 processBuilder.command("python", "tmp.py");
                 Process process = processBuilder.start();
                 StringBuilder output = new StringBuilder();
+                output.append("```");
                 StringBuilder errorOut = new StringBuilder();
+                errorOut.append("```");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
                 BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 
@@ -46,6 +69,8 @@ public class EvalCommand implements ICommand{
                 while ((line = errorReader.readLine()) != null) {
                     errorOut.append(line).append("\n");
                 }
+                errorOut.append("```");
+                output.append("```");
 
                 int exitVal = process.waitFor();
                 if (exitVal == 0) {
