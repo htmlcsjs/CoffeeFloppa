e75578d694b4ca87b0f989d40c78fc26fa0ca808
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-05-15 13:41:37 +0100

    Improve the formatting of material commands

--- src/main/java/net/htmlcsjs/coffeeFloppa/helpers/MaterialCommandsHelper.java
+++ src/main/java/net/htmlcsjs/coffeeFloppa/helpers/MaterialCommandsHelper.java
@@ -13,22 +13,22 @@ public class MaterialCommandsHelper {
 
     public static String parseMaterial(Map<String, Object> materialMap) {
         StringBuilder returnMsg = new StringBuilder();
-        returnMsg.append(String.format("Name: %s\n", ((String) materialMap.get("unlocalized_name")).split("\\.")[1]));
-        returnMsg.append(String.format("Colour: #%x\n", (long) materialMap.get("colour")));
-        returnMsg.append(String.format("ID: %d\n", (long) materialMap.get("id")));
-        returnMsg.append(String.format("Mass: %d\n", (long) materialMap.get("mass")));
-        returnMsg.append(String.format("Icon Set: %s\n", materialMap.get("icon_set")));
+        returnMsg.append(String.format("Name: `%s`\n", ((String) materialMap.get("unlocalized_name")).split("\\.")[1]));
+        returnMsg.append(String.format("Colour: `#%x`\n", (long) materialMap.get("colour")));
+        returnMsg.append(String.format("ID: `%d`\n", (long) materialMap.get("id")));
+        returnMsg.append(String.format("Mass: `%d`\n", (long) materialMap.get("mass")));
+        returnMsg.append(String.format("Icon Set: `%s`\n", materialMap.get("icon_set")));
         Map<String, Object> propertiesMap = (Map<String, Object>) materialMap.get("properties");
         for (String propertyName: propertiesMap.keySet()) {
             returnMsg.append(propertyName.substring(0, 1).toUpperCase())
-                    .append(String.join(" ", propertyName.substring(1).split("\\_"))).append(": ").
-                    append(propertiesMap.get(propertyName).toString())
-                    .append("\n");
+                    .append(String.join(" ", propertyName.substring(1).split("_"))).append(": `")
+                    .append(propertiesMap.get(propertyName).toString())
+                    .append("`\n");
         }
         List<Map<String, Object>> componentsList = (List<Map<String, Object>>) materialMap.get("components");
+        returnMsg.append("Formula: `").append(materialMap.get("formula")).append("`\n");
         if (!componentsList.isEmpty()) {
-            returnMsg.append("Formula: ").append(materialMap.get("formula")).append("\n");
-            returnMsg.append("Composed of: \n");
+            returnMsg.append("\nComposed of: ```java\n");
             for (Map<String, Object> component: componentsList) {
                 returnMsg.append(" - ")
                         .append(component.get("amount"))
@@ -36,6 +36,7 @@ public class MaterialCommandsHelper {
                         .append(((String) component.get("name")).split("\\.")[1])
                         .append("\n");
             }
+            returnMsg.append("```");
         }
         return returnMsg.toString();
     }
