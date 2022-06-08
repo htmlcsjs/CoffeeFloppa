a2efa1ed2ab5457e41d426864f162f53aaec6940
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-06-08 10:13:02 +0100

    add switch case for what mod materials are from

--- src/main/java/net/htmlcsjs/coffeeFloppa/helpers/MaterialCommandsHelper.java
+++ src/main/java/net/htmlcsjs/coffeeFloppa/helpers/MaterialCommandsHelper.java
@@ -56,7 +56,7 @@ public class MaterialCommandsHelper {
 
         EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder().color(Color.of(((Long) materialInfo.getOrDefault("colour", 0x36393f)).intValue()))
                 .title(name)
-                .author(String.format("ID: %d", (long) materialInfo.get("id")), null, "https://cdn.discordapp.com/emojis/924907233618329640.png")
+                .addField("ID", String.format("ID: %d", (long) materialInfo.get("id")), true)
                 .addField("Mass", String.valueOf(materialInfo.get("mass")), true)
                 .addField("Icon Set", (String) materialInfo.get("icon_set"), true)
                 .addField("Colour", String.format("#%x", (long) materialInfo.get("colour")), true);
@@ -84,6 +84,47 @@ public class MaterialCommandsHelper {
             }
             compBuilder.append("```");
             builder.addField("Composition", compBuilder.toString(), false);
+        }
+
+        long id = (long) materialInfo.get("id");
+        if (id <= 2999) {
+            builder.author("GregTech CEu",
+                    "https://www.curseforge.com/minecraft/mc-mods/gregtech-ce-unofficial/",
+                    "https://media.forgecdn.net/avatars/468/148/637751129280745336.png");
+        } else if (id <= 3059) {
+            builder.author("Gregicality Multiblocks",
+                    "https://www.curseforge.com/minecraft/mc-mods/gregicality-multiblocks",
+                    "https://media.forgecdn.net/avatars/476/750/637770403565836783.png");
+        } else if (id <= 19999) {
+            builder.author("Gregicality Science",
+                    "https://github.com/GregTechCEu/gregicality-science",
+                    "https://cdn.discordapp.com/emojis/949194079500120154.gif");
+        } else if (id <= 20999) {
+            builder.author("Gregification",
+                    "https://github.com/GregTechCEu/Gregification",
+                    "https://cdn.discordapp.com/attachments/904846945095385138/984013433584353280/gregification_logo.png");
+        } else if (id <= 21499) {
+            builder.author("HtmlTech",
+                    "https://www.curseforge.com/minecraft/mc-mods/htmltech",
+                    "https://media.forgecdn.net/avatars/471/74/637758777197994901.png");
+        } else if (id <= 21999) {
+            builder.author("GregTech Food Option",
+                    "https://www.curseforge.com/minecraft/mc-mods/gregtech-food-option",
+                    "https://media.forgecdn.net/avatars/376/445/637552985536571351.png");
+        } else if (id <= 23599) {
+            builder.author("PCM's Ore Addon", null, null);
+        } else if (id <= 23999) {
+            builder.author("MechTech",
+                    "https://www.curseforge.com/minecraft/mc-mods/mechtech",
+                    "https://media.forgecdn.net/avatars/360/897/637525560735345110.png");
+        } else if (id >= 32000) {
+            builder.author("CraftTweaker Material",
+                    null,
+                    "https://media.forgecdn.net/avatars/142/108/636546700830987709.png");
+        } else {
+            builder.author("Custom",
+                    null,
+                    "https://raw.githubusercontent.com/GregTechCEu/GregTech/master/src/main/resources/assets/gregtech/textures/items/tools/wrench.png");
         }
 
         returnList.add(builder.build());
