0511e9bcd7ae863306da498fe84b474d976ab976
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-06-17 17:37:14 +0100

    finish oc impl

--- src/main/java/net/htmlcsjs/coffeeFloppa/commands/OcCommand.java
+++ src/main/java/net/htmlcsjs/coffeeFloppa/commands/OcCommand.java
@@ -1,6 +1,8 @@
 package net.htmlcsjs.coffeeFloppa.commands;
 
 import discord4j.core.object.entity.Message;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.AllowedMentions;
 import gregtech.api.GTValues;
 import gregtech.api.recipes.logic.OverclockingLogic;
 import gregtech.api.util.GTUtility;
@@ -11,6 +13,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OcCommand implements ICommand {
+
+    private static final String aboutStr = "This calculates oc according to gt's logic";
+    private static final String errorStr = "must be formatted as eut, duration, and have the flag `--ticks` if you want answers in ticks";
+
     @Override
     public @NotNull String getName() {
         return "oc";
@@ -46,16 +52,28 @@ public class OcCommand implements ICommand {
                     outputList.add(sus);
                 }
 
-                StringBuilder outputStr = new StringBuilder("```lua\n");
+                StringBuilder eutStr = new StringBuilder("```lua\n");
+                StringBuilder timeStr = new StringBuilder("```lua\n");
+                StringBuilder voltageStr = new StringBuilder("```lua\n");
                 for (String[] strs :outputList) {
-                    outputStr.append(String.join(" | ", strs)).append("\n");
+                    eutStr.append(strs[0]).append("\n");
+                    timeStr.append(strs[1]).append("\n");
+                    voltageStr.append(strs[2]).append("\n");
                 }
-                return outputStr.append("```").toString();
+                String time = ticks ? String.format("%,ft", duration) : String.format("%,.2fs", duration / 20);
+                message.getChannel().flatMap(channel -> channel.createMessage().withEmbeds(
+                                EmbedCreateSpec.builder().title(String.format("%,d EU/t (%s) for (%s)", eut, GTValues.VN[startingVoltage], time))
+                                        .addField("EU/t", eutStr.append("```").toString(), true)
+                                        .addField("Time", timeStr.append("```").toString(), true)
+                                        .addField("Voltage", voltageStr.append("```").toString(), true)
+                                        .build()
+                        ).withMessageReference(message.getId())
+                        .withAllowedMentions(AllowedMentions.suppressEveryone())).subscribe();
+                return null;
             }
         } catch (IndexOutOfBoundsException ignored) {
             epicFormattingFail = true;
         }
-        String errorStr = "must be formatted as eut, duration, and have the flag `--ticks` if you want answers in ticks";
-        return errorStr;
+        return epicFormattingFail ? aboutStr + "\n\n" + errorStr : aboutStr;
     }
 }
