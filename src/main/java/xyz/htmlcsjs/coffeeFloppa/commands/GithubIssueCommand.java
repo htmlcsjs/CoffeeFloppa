fe3e8c438f4db864c0338c00c08029650bebfea6
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2023-08-01 15:43:51 +0100

    make the list of issues compact when more than 2 are referanced

--- src/main/java/xyz/htmlcsjs/coffeeFloppa/commands/GithubIssueCommand.java
+++ src/main/java/xyz/htmlcsjs/coffeeFloppa/commands/GithubIssueCommand.java
@@ -68,44 +68,50 @@ public class GithubIssueCommand implements ICommand {
     @Nullable
     @Override
     public String execute(Message message) {
-        Map<String, String> issueMap = new HashMap<>();
+        List<IssueListItem> issueList = new ArrayList<>();
         CommandUtil.ghIssuePattern.matcher(message.getContent())
                 .results()
-                .limit(5)
-                .forEach(matchResult -> issueMap.put(matchResult.group(1), matchResult.group(2)));
+                .forEach(matchResult -> issueList.add(new IssueListItem(matchResult.group(1), matchResult.group(2))));
 
         CommandUtil.ghIssueSmallPattern.matcher(message.getContent())
                 .results()
-                .limit(5 - issueMap.size())
                 .forEach(matchResult -> {
                     if (matchResult.group(1) == null) {
                         MessageChannel channel = message.getChannel().block();
                         if (channel != null && categoryAliasMap.containsKey(channel.getId().asString())) {
-                            issueMap.put(aliasMap.get(categoryAliasMap.get(channel.getId().asString())), matchResult.group(2));
+                            issueList.add(new IssueListItem(aliasMap.get(categoryAliasMap.get(channel.getId().asString())), matchResult.group(2)));
                         } else if (channel instanceof ThreadChannel thread) {
                             Optional<Snowflake> parentId = thread.getParentId();
                             if (parentId.isPresent() && categoryAliasMap.containsKey(parentId.get().asString())) {
-                                issueMap.put(aliasMap.get(categoryAliasMap.get(parentId.get().asString())), matchResult.group(2));
+                                issueList.add(new IssueListItem(aliasMap.get(categoryAliasMap.get(parentId.get().asString())), matchResult.group(2)));
                             }
                         }
                     } else if (aliasMap.containsKey(matchResult.group(1))){
-                        issueMap.put(aliasMap.get(matchResult.group(1)), matchResult.group(2));
+                        issueList.add(new IssueListItem(aliasMap.get(matchResult.group(1)), matchResult.group(2)));
                     }
                 });
 
-        HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).followRedirects(HttpClient.Redirect.ALWAYS).connectTimeout(Duration.ofSeconds(5)).build();
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .connectTimeout(Duration.ofSeconds(5)).build();
         List<EmbedCreateSpec> embeds = new ArrayList<>();
+        StringBuilder msgBody = new StringBuilder();
 
-        if (issueMap.isEmpty()) {
+        if (message.getContent().startsWith(String.format("$%s", getName())) && issueList.isEmpty()) {
             return "No valid issue descriptors supplied\nUse the format `owner/repo#issueid` or `alias#issueid`";
+        } else if (issueList.isEmpty()) {
+            return null;
         }
 
-        issueMap.forEach((repo, id) -> {
+        for (IssueListItem item : issueList) {
+            String repo = item.repo;
+            String id = item.id;
             if (!repo.matches("[\\w.-]+/[\\w.-]+")) {
                 if (aliasMap.containsKey(repo)) {
                     repo = aliasMap.get(repo);
                 } else {
-                    return;
+                    continue;
                 }
             }
 
@@ -117,12 +123,8 @@ public class GithubIssueCommand implements ICommand {
                 HttpResponse<String> response = client.send(builder.build(), HttpResponse.BodyHandlers.ofString());
                 JSONObject issueJSON = (JSONObject) new JSONParser().parse(response.body());
                 if (response.statusCode() != 200) {
-                    embeds.add(EmbedCreateSpec.builder()
-                            .title(String.format("Couldn't find issue %s in repo %s", id, repo))
-                            .addField("status", String.format("`%d`", response.statusCode()), true)
-                            .addField("message", String.format("`%s`", issueJSON.get("message")), true)
-                            .build());
-                } else {
+                    msgBody.append(String.format("Couldn't find issue %s in repo `%s`. HTTP status: `%d`. Message: `%s`\n", id, repo, response.statusCode(), issueJSON.get("message")));
+                } else if (issueList.size() <= 2) {
                     EmbedCreateSpec.Builder embedBuilder = EmbedCreateSpec.builder()
                             .title(Possible.of((String) issueJSON.get("title")))
                             .url((String) issueJSON.get("html_url"));
@@ -133,7 +135,7 @@ public class GithubIssueCommand implements ICommand {
                         StringBuilder body = new StringBuilder();
                         int i = 0;
                         for (String s : rawBody.split("\n")) {
-                            if (i > 10) {
+                            if (i > 5) {
                                 body = new StringBuilder(body.toString().trim() + "...\n");
                                 break;
                             }
@@ -146,20 +148,32 @@ public class GithubIssueCommand implements ICommand {
                     }
                     embedBuilder.timestamp(Instant.parse((String) issueJSON.get("updated_at")));
                     embeds.add(embedBuilder.build());
+                } else {
+                    msgBody.append("[#%s: %s](<https://github.com/%s/issues/%s>\n".formatted(id, issueJSON.get("title"), repo, id));
                 }
             } catch (IOException | InterruptedException | ParseException e) {
                 CoffeeFloppa.handleException(e);
             }
-        });
+        }
+
+        if (!msgBody.isEmpty()) {
+            msgBody = new StringBuilder().append("Issues referenced:\n").append(msgBody);
+        }
+
+        StringBuilder finalMsgBody = msgBody;
         MessageHandler.sendRegisterMessage(message, message.getChannel().flatMap(channel -> channel.createMessage()
                 .withEmbeds(embeds)
                 .withMessageReference(message.getId())
-                .withAllowedMentions(AllowedMentions.suppressEveryone())));
+                .withAllowedMentions(AllowedMentions.suppressEveryone())
+                .withContent(finalMsgBody.toString())));
         return null;
     }
 
     @Override
     public String helpInfo() {
         return "Provides links to issues for github repos";
+    }
+    private record IssueListItem(String repo, String id) {
+
     }
 }
